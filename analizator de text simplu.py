16fa2d5952d6b0a5acbe3b6987187a68c64fe75a
Blaga Alexandra <blagaalexandramaria@gmail.com>
2026-03-17 10:15:53 +0200

    Improved stop words filtering (added English function words)

--- analizator de text simplu.py
+++ analizator de text simplu.py
@@ -71,34 +71,35 @@ STOP_WORDS = {
     "să", "mai", "care", "că", "sau", "pentru", "după",
 
     # Modal verbs & auxiliaries
-"may","might","must","can","could","shall","should","will","would",
+    "may","might","must","can","could","shall","should","will","would",
 
-# Pronouns
-"i","you","he","she","it","we","they","me","him","her","them",
-"my","your","his","their","our","its","ours","yours","theirs",
+    # Pronouns
+    "i","you","he","she","it","we","they","me","him","her","them",
+    "my","your","his","their","our","its","ours","yours","theirs",
+    "which"
 
-# Articles & determiners
-"a","an","the","this","that","these","those",
+    # Articles & determiners
+    "a","an","the","this","that","these","those",
 
-# Prepositions
-"in","on","at","by","for","with","about","against","between","into",
-"through","during","before","after","above","below","from","up","down",
-"out","off","over","under","again","further","within","without",
+    # Prepositions
+    "in","on","at","by","for","with","about","against","between","into",
+    "through","during","before","after","above","below","from","up","down",
+    "out","off","over","under","again","further","within","without",
 
-# Conjunctions
-"and","or","but","so","because","although","if","while","where","when",
+    # Conjunctions
+    "and","or","but","so","because","although","if","while","where","when",
 
-# Common verbs
-"is","are","was","were","be","been","being",
-"have","has","had",
-"do","does","did",
+    # Common verbs
+    "is","are","was","were","be","been","being",
+    "have","has","had",
+    "do","does","did",
 
-# Very common useless words
-"not","no","yes","very","just","more","most","such","only","own","same",
-"too","than","then","also","even","still","yet",
+    # Very common useless words
+    "not","no","yes","very","just","more","most","such","only","own","same",
+    "too","than","then","also","even","still","yet",
 
-# Extra (foarte importante pentru tine)
-"may","one","two","first","second","new","many","much"
+    # Extra (foarte importante pentru tine)
+    "may","one","two","first","second","new","many","much"
 }
 
 
